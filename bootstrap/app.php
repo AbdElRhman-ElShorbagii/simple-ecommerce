54d3c9efc30d7ceef7622a86dcd4975d756169c0
Abdelrhman Elshorbagii <abdelrhmanelhorbagii@gmail.com>
2025-06-01 14:52:03 +0300

    updated app.php to bootstrap Sanctum & Events

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Events\OrderPlaced;
+use App\Listeners\SendOrderPlacedNotification;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -7,12 +9,25 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // Add Sanctum middleware to API routes
+        $middleware->api(prepend: [
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        ]);
+
+        // Configure stateful domains for Sanctum (if using SPA)
+        $middleware->statefulApi();
+
     })
+    ->withEvents([
+        OrderPlaced::class => [
+            SendOrderPlacedNotification::class,
+        ],
+    ])
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
