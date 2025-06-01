ba97dc279b8ce8e5b81c42b8874912110e28f66a
Abdelrhman Elshorbagii <abdelrhmanelhorbagii@gmail.com>
2025-06-01 21:15:26 +0300

    added login page

--- resources/js/pages/Login.jsx
+++ resources/js/pages/Login.jsx
@@ -1,13 +1,67 @@
 import React from 'react';
-import FilterFloatingButton from '../components/FilterFloatingButton';
-import { Typography } from '@mui/material';
+import {
+  Box,
+  TextField,
+  Button,
+  Typography,
+  Container,
+  CssBaseline,
+} from '@mui/material';
 
 const Login = () => {
-    return (
-        <div>
-            <Typography>ddd</Typography>
-        </div>
-    );
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          padding: 4,
+          borderRadius: 2,
+          boxShadow: 3,
+          backgroundColor: 'white',
+          marginTop: '20vh',
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Welcome back
+        </Typography>
+        <Typography variant="body2" color="text.secondary" align="center" marginBottom={2}>
+          Please enter your details to sign in
+        </Typography>
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Enter your email"
+          name="email"
+          autoComplete="email"
+          autoFocus
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Enter your password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Login
+        </Button>
+      </Box>
+    </Container>
+  );
 };
 
 export default Login;
