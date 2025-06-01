a18e5992a2f9697c2e104e0d7b5c39febd4b25eb
Abdelrhman Elshorbagii <abdelrhmanelhorbagii@gmail.com>
2025-06-01 19:04:41 +0300

    updated app header design

--- resources/js/components/AppHeader.jsx
+++ resources/js/components/AppHeader.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
-import { AppBar, Toolbar, Button, IconButton, Box } from '@mui/material';
+import { AppBar, Toolbar, Button, IconButton, Box, Container } from '@mui/material';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { Link } from 'react-router-dom';
 
 const AppBarComponent = () => (
   <AppBar position="static" color="default" elevation={1}>
-    <Toolbar sx={{ justifyContent: 'space-between' }}>
-      <Box display="flex" alignItems="center" gap={1}>
-        <img src="/logo.png" alt="Izam" height="30" />
-        <Button variant="text">Products</Button>
-        <Button variant="contained">Sell Your Product</Button>
-      </Box>
+    <Container>
+        <Toolbar sx={{ justifyContent: 'space-between' }}>
+        <Box display="flex" alignItems="center" gap={1}>
+            <img src="/logo.png" alt="Izam" height="30" />
+            <Button variant="text" component={Link} to="/">Products</Button>
+            <Button variant="contained">Sell Your Product</Button>
+        </Box>
 
-      <Box display="flex" alignItems="center" gap={1}>
-        <IconButton color="inherit">
-          <ShoppingCartIcon />
-        </IconButton>
-        <Button variant="contained">Login</Button>
-      </Box>
-    </Toolbar>
+        <Box display="flex" alignItems="center" gap={1}>
+            <IconButton color="inherit" component={Link} to="/cart">
+            <ShoppingCartIcon />
+            </IconButton>
+            <Button variant="contained" component={Link} to="/login">Login</Button>
+        </Box>
+        </Toolbar>
+    </Container>
   </AppBar>
 );
 
